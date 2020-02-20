b9a3b8f3c7a5d7ebd663ff3bab7474df25b9d969
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-02-20 14:10:41 -0300

    Created banner and start with button

---
+++ src/components/Banner/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export default function Banner() {
+	return(
+		<div className="bannerImg">
+			<button></button>
+		</div>
+	);
+}
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './main.css';
 import Footer from "../../components/Footer";
+import Banner from "../../components/Banner";
 
 export default function Main() {
 	return (
 		<>
+			<Banner />
 			<Footer />
 		</>
 	);
