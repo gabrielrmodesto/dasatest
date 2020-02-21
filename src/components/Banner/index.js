cb15d0c9694c374b7a87f5d87ab25119c1c73160
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-02-20 23:36:54 -0300

    Images and start post position

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -4,12 +4,18 @@ import "./style.css";
 export default function Banner() {
 	return (
 		<div className="bannerImg">
-			<div className="headerTitle">
-				<h1>Dreamshare</h1>
-			</div>
-			<div className="buttonHeader">
-				<button>Login</button>
-				<button>Sign up</button>
+			<header>
+				<div className="headerTitle">
+					<h1>Dreamshare</h1>
+					<button>Login</button>
+					<button>Sign up</button>
+				</div>
+			</header>
+			
+			<div className="contentBanner">
+				<h1>Share Your Holiday Dream</h1>
+				<p>And find the perfect partner to fullfill it</p>
+				<button>Find your holiday partner</button>
 			</div>
 		</div>
 	);
