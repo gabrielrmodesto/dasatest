aa0c6f2aed21f3cca600d0fe296c11b7a958c859
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-02-21 00:10:30 -0300

    position and style to header and butttons header

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -6,12 +6,20 @@ export default function Banner() {
 		<div className="bannerImg">
 			<header>
 				<div className="headerTitle">
-					<h1>Dreamshare</h1>
-					<button>Login</button>
-					<button>Sign up</button>
+					<div className="title">
+						<h1>Dreamshare</h1>
+					</div>
+					<div className="btns">
+						<button className="loginBtn" type="button">
+							Log in
+						</button>
+						<button className="signBtn" type="button">
+							Sign up
+						</button>
+					</div>
 				</div>
 			</header>
-			
+
 			<div className="contentBanner">
 				<h1>Share Your Holiday Dream</h1>
 				<p>And find the perfect partner to fullfill it</p>
